1c27d47aa400d3790f0322c9de76c6e690ee8b75
Sithmi S <139143701+Sithmi-Shehara@users.noreply.github.com>
2025-04-26 19:53:54 +0530

    Add files via upload
    Post Data in Mongodb database and  Fetch data by ID in MongoDB database

---
+++ PafMongoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.PAFMongoSpring.PAFMongoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafMongoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafMongoSpringApplication.class, args);
+	}
+
+}
