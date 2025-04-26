0846cecca94f19997749f7997f1dba1448890a4d
Yuwani Rashipaba <it22343352@my.sliit.lk>
2025-04-26 23:07:32 +0530

    added notification respository

--- src/main/java/com/example/PAF/repository/NotificationRepository.java
+++ src/main/java/com/example/PAF/repository/NotificationRepository.java
@@ -1,4 +1,7 @@
 package com.example.PAF.repository;
 
-public class NotificationRepository {
+import com.example.PAF.model.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NotificationRepository extends MongoRepository<Notification, Integer>  {
 }
