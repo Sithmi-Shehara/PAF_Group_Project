09af147ceebad22d722f8dbf70ad74f2978254d4
yuwanirashipaba <it22343352@my.sliit.lk>
2025-05-04 10:19:46 +0530

    added headline to update post dto

--- src/main/java/com/example/PAF/dtos/PostUpdateRequest.java
+++ src/main/java/com/example/PAF/dtos/PostUpdateRequest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @Data
 public class PostUpdateRequest {
+    private String headline;
     private String description;
     private String title;
     private MultipartFile mediaFile0;
