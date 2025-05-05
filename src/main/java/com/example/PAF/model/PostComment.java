7539b2844ea594b133c47025ab3cfb08ec17cf06
IT22343420 <127926168+IT22343420@users.noreply.github.com>
2025-05-05 20:48:12 +0530

    create model for post comment

---
+++ src/main/java/com/example/PAF/model/PostComment.java
@@ -0,0 +1,57 @@
+package com.example.PAF.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "post-comments")
+public class PostComment {
+
+    @Id
+    private String id;
+    private String content;
+    private String userName;
+    private String postId;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
