1e98208ec31cf6b615d9e01aed0cc84d30812675
IT22343420 <127926168+IT22343420@users.noreply.github.com>
2025-05-05 21:12:53 +0530

    create controllers for manage comments

---
+++ src/main/java/com/example/PAF/controller/CommentController.java
@@ -0,0 +1,28 @@
+package com.example.PAF.controller;
+
+import com.example.PAF.dtos.CommentRequest;
+import com.example.PAF.model.PostComment;
+import com.example.PAF.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping("/add")
+    public ResponseEntity<PostComment> addComment(@RequestBody CommentRequest request) {
+        return ResponseEntity.ok(commentService.addComment(request));
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<List<PostComment>> getCommentsByPost(@PathVariable String postId) {
+        return ResponseEntity.ok(commentService.getCommentsByPostId(postId));
+    }
+}
