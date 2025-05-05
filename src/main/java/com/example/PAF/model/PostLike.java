cd27c104de1ecf07fee53116c0bd63f29d139678
IT22343420 <127926168+IT22343420@users.noreply.github.com>
2025-05-05 21:18:20 +0530

    add postlike model for store post like data

---
+++ src/main/java/com/example/PAF/model/PostLike.java
@@ -0,0 +1,46 @@
+package com.example.PAF.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "post_likes")
+public class PostLike {
+
+    @Id
+    private String id;
+    private String postId;
+    private String userName;
+
+    public PostLike() {}
+
+    public PostLike(String postId, String userName) {
+        this.postId = postId;
+        this.userName = userName;
+    }
+
+    // Getters & Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
