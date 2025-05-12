df82744961cc5924d0dcf946b3ceddb44dccd1bf
yuwanirashipaba <it22343352@my.sliit.lk>
2025-05-13 00:03:23 +0530

    saved notification when someone liked a post

--- src/main/java/com/example/PAF/service/LikeServiceImpl.java
+++ src/main/java/com/example/PAF/service/LikeServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.PAF.service;
 
 import com.example.PAF.dtos.LikeRequest;
+import com.example.PAF.model.Notification;
+import com.example.PAF.model.Post;
 import com.example.PAF.model.PostLike;
 import com.example.PAF.repository.LikeRepository;
+import com.example.PAF.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,7 +20,7 @@ public class LikeServiceImpl implements LikeService {
     @Autowired
     private NotificationService notificationService;
     @Autowired
-    private PostService postService;
+    private PostRepository postRepository;
 
     @Override
     public String toggleLike(LikeRequest request) {
@@ -28,6 +32,14 @@ public class LikeServiceImpl implements LikeService {
         } else {
             PostLike newLike = new PostLike(request.getPostId(), request.getUserName());
             likeRepository.save(newLike);
+
+            Notification notification = new Notification();
+            notification.setUserName(request.getUserName());
+            notification.setCreatedAt(new Date());
+            notification.setTitle("New Post Liked");
+            Post post = postRepository.findById(request.getPostId());
+            notification.setDescription(request.getUserName() + " liked the post about : " + post.getTitle());
+            notificationService.addNotification(notification);
             return "Like added";
         }
     }
