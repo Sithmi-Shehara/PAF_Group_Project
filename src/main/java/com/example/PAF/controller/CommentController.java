b2d7c94090d5439b8d336f0b562bd3a4382ec10e
IT22343420 <127926168+IT22343420@users.noreply.github.com>
2025-05-13 12:51:37 +0530

    fixed a bug in comment controller

--- src/main/java/com/example/PAF/controller/CommentController.java
+++ src/main/java/com/example/PAF/controller/CommentController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//created commentController
 @RestController
 @RequestMapping("/api/comments")
 public class CommentController {
