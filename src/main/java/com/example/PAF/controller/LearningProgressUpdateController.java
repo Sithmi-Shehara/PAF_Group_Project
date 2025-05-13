b342d012cc77fcc8a0a26c2c003c55c2690ae77a
IT22343420 <127926168+IT22343420@users.noreply.github.com>
2025-05-13 13:26:06 +0530

    fixed a bug in learning progress update

--- src/main/java/com/example/PAF/controller/LearningProgressUpdateController.java
+++ src/main/java/com/example/PAF/controller/LearningProgressUpdateController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-//created learning progress update controller
+    // created learning progress update controller
 @RestController
 @RequestMapping("/api/progress")
 @CrossOrigin
