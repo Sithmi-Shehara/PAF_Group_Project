06cbc3636dfa2fc915c002ff5f02357d79af1f4f
IT22343420 <127926168+IT22343420@users.noreply.github.com>
2025-05-13 12:42:41 +0530

    changed the constructor

--- src/main/java/com/example/PAF/model/PostLike.java
+++ src/main/java/com/example/PAF/model/PostLike.java
@@ -5,7 +5,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "post_likes")
 public class PostLike {
-//created postLike model
+    //created postLike model
+
     @Id
     private String id;
     private String postId;
