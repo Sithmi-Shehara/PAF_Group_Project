e890a624d1de8e31ce483840cfc572470652f3b4
IT22343420 <127926168+IT22343420@users.noreply.github.com>
2025-05-13 12:10:52 +0530

    change attribute name in get title

--- src/main/java/com/example/PAF/model/LearningProgressUpdate.java
+++ src/main/java/com/example/PAF/model/LearningProgressUpdate.java
@@ -46,6 +46,7 @@ public class LearningProgressUpdate {
     }
 
     public String getTitle() { return title; }
+
     public void setTitle(String title) { this.title = title; }
 
     public String getContent() { return content; }
